4c3972af0e3f470dc9df8010f2c3aadec76431d9
Mandzheev <mandzheevdmitriy@gmail.com>
2021-06-13 18:59:46 +0500

    1

--- calculator.cxx
+++ calculator.cxx
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <iostream>
 using namespace std;
 
@@ -6,6 +7,9 @@ using namespace std;
 void error(string s) { throw runtime_error(s); }
 
 /******************************************************/
+const char number = '8';
+const char quit = 'q';
+const char print = '=';
 
 class Token {
 public:
@@ -41,16 +45,17 @@ Token_stream::Token_stream()
 
 // Puts its argument back into the Token_stream's buffer:
 void Token_stream::putback(Token t) {
-  if (full) error("putback() into a full buffer");
-  buffer = t; // copy t to buffer
-  full = true; //buffer is full
+  if (full)
+    error("putback() into a full buffer");
+  buffer = t;  // copy t to buffer
+  full = true; // buffer is full
 }
 
 /******************************************************/
 
 Token Token_stream::get() {
   if (full) {
-    full = false; //remove token from buffer
+    full = false; // remove token from buffer
     return buffer;
   }
 
@@ -58,17 +63,30 @@ Token Token_stream::get() {
   cin >> ch;
 
   switch (ch) {
-  case '=': case 'q': case '(': case ')': // 'q' for quit
-  case '+': case '-': case '*': case '/':
+  case print:
+  case quit:
+  case '(':
+  case ')': // 'q' for quit
+  case '+':
+  case '-':
+  case '*':
+  case '/':
     return Token{ch};
-  case '.':
-  case '0': case '1': case '2': case '3': case '4':
-  case '5':case '6': case '7': case '8': case '9':
-  {
-    cin.putback(ch); // put digit into the buffer
+  case '.': // a floating-point-literal can start with a dot
+  case '0':
+  case '1':
+  case '2':
+  case '3':
+  case '4':
+  case '5':
+  case '6':
+  case '7':
+  case '8':
+  case '9': {
+    cin.putback(ch); // put digit into the input stream
     double val;
     cin >> val; // read a floating-point number
-    return Token{'8', val};  // let '8' represent "a number"
+    return Token{number, val};
   }
   default:
     error("Bad token");
@@ -77,7 +95,7 @@ Token Token_stream::get() {
 
 /******************************************************/
 
-Token_stream ts; // provides get() and putback() 
+Token_stream ts; // provides get() and putback()
 
 /******************************************************/
 
@@ -88,7 +106,7 @@ double expression() {
 
   double left = term(); // read and evaluate term()
 
-  Token t = ts.get(); // read and evaluate a Term
+  Token t = ts.get(); 
 
   while (true) {
     switch (t.kind) {
@@ -103,7 +121,7 @@ double expression() {
       break;
     default:
       ts.putback(t); // put t back into the token stream
-      return left; // finally: return the answer
+      return left;   // finally: return the answer
     }
   }
 }
@@ -124,8 +142,12 @@ double primary() {
       error("')'expected");
     return d;
   }
-  case '8': // '8' to represent a number
+  case number:
     return t.value;
+  case '-':
+    return -primary();
+  case '+':
+    return primary();
   default:
     error("primary expected");
   }
@@ -153,6 +175,7 @@ double term() {
       t = ts.get();
       break;
     }
+    case '%':
     default:
       ts.putback(t);
       return left;
@@ -161,26 +184,31 @@ double term() {
 }
 
 /******************************************************/
+void calculate() {
+  double val = 0;
+
+  while (cin) {
+    Token t = ts.get();
+
+    if (t.kind == quit)
+      break;
+    if (t.kind == print)
+      cout << "=" << val << '\n';
+    else
+      ts.putback(t);
+    val = expression();
+  }
+}
+/******************************************************/
 
 int main() try {
   cout << "Simple calculator.\n";
   cout << "[q]quit, [=]result\n";
   cout << "Enter expressions:\n";
 
-  double val = 0;
-
-  while (cin) {
-    Token t = ts.get();
-
-    if (t.kind == 'q')
-      break;
-    if (t.kind == '=')
-      cout << "=" << val << '\n';
-    else
-      ts.putback(t);
-    val = expression();
-  }
-} catch (exception &e) {
+  calculate();
+  return 0;
+} catch (runtime_error &e) {
   cerr << e.what() << '\n';
   return 1;
 } catch (...) {
